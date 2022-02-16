be6f9e4648f9fd9e13afbde379e6a451222e2bff
203 <203@academicos.cesde.edu.co>
2022-02-16 10:03:20 -0500

    Carpeta de js

---
+++ js/controladorTienda.js
@@ -0,0 +1,33 @@
+//Comentario de linea
+// Entradas del problema
+/*
+Comentarios en
+Bloque
+*/
+
+/*Entradas == Variables == Constantes*/
+console.log("quiubo neaaaaa");
+let nombreProducto="carrostyle"
+let precioProducto=400000
+let pesoProducto=18.5
+let tieneDescuento = false
+let descripcionProducto = "Un carro con mucho estilo 24/7 bb"
+let telefonoContacto = "85222561"
+
+const NOMBRE_CLIENTE="Juan Florez"
+
+//Salidas 
+console.log("Buenas")
+console.log(NOMBRE_CLIENTE)
+console.log("El producto es : "+nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es : ${nombreProducto}`)
+
+/*Arreglos unidimen sionales (Vectores)
+Sirven para almacenar varios elementos en una sola variable */
+
+let productos=["Carrostyle","Carro nea","El carro del bryan", "Carro fresa", "Pupy carro"]
+let productos2=Array("Comida 1", "comida 2", "comida 3 ")
+
+console.log(productos)
+console.log(productos2[0])
+
