47118bfcd7579b2a2c526e03fcf3e2718313458a
Juan <juanpabloramirezflorez@gmail.com>
2022-03-01 19:38:29 -0500

    Desatrazo que tuve por ir a buscar practicas

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,33 +1,74 @@
-//Comentario de linea
-// Entradas del problema
 /*
-Comentarios en
-Bloque
+// Entrada de problema
+let nombreProducto ="batimovil"
+let precioProducto ="400000"
+let pesoProducto = 18.5
+let tieneDescuento = true
+let descripcionProducto ="Batimovil a escala"
+let telefonoVendedor ="1234567"
+
+const NOMBRE_CLIENTE="Luis"
+
+// Arreglos unidemencionales
+// Sirven para almacenar varios elementos en una sola variables
+let productos=["batimovil","camiseta","gorra","bati morral"]
+let productos2=Array("batimovil","camiseta","gorra","bati morral")
+
+
+//Salidas
+console.log("Buenas tardes")
+console.log(nombreProducto)
+console.log("Su nombre es: "+NOMBRE_CLIENTE+ " y su producto es: "+nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es: ${nombreProducto}`)
+console.log("************")
+console.log(productos)
+console.log(productos2[2])
+console.log(productos2[3])
+console.log(productos2[1])
 */
 
-/*Entradas == Variables == Constantes*/
-console.log("quiubo neaaaaa");
-let nombreProducto="carrostyle"
-let precioProducto=400000
-let pesoProducto=18.5
-let tieneDescuento = false
-let descripcionProducto = "Un carro con mucho estilo 24/7 bb"
-let telefonoContacto = "85222561"
 
-const NOMBRE_CLIENTE="Juan Florez"
+/*
+//controlando etiquetas
 
-//Salidas 
-console.log("Buenas")
-console.log(NOMBRE_CLIENTE)
-console.log("El producto es : "+nombreProducto)
-console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es : ${nombreProducto}`)
+//1. Crear una varible para guardar una etiqueta
+let etiquetaTitulo = document.getElementById("titulo")
+let etiquetaMenu = document.getElementById("PeakyBlinders")
+console.log(etiquetaTitulo)
+console.log(etiquetaMenu)
 
-/*Arreglos unidimen sionales (Vectores)
-Sirven para almacenar varios elementos en una sola variable */
 
-let productos=["Carrostyle","Carro nea","El carro del bryan", "Carro fresa", "Pupy carro"]
-let productos2=Array("Comida 1", "comida 2", "comida 3 ")
+//Como modificar una etiqueta
+etiquetaTitulo.textContent = "Rojo vos no existis"
+etiquetaMenu.textContent = "PeakyBlinders"
 
-console.log(productos)
-console.log(productos2[0])
+//Controlando una imagen
+let etiquetafoto = document.getElementById("fototommy")
+etiquetafoto.src="img/tommy3.jpg"
 
+//Cambiar el estilo de a etiqueta
+etiquetaTitulo.classList.add("text-center")
+etiquetaTitulo.classList.add("text-danger")
+*/
+
+//Tienda
+let productosNombre=["CamisetaNegra","CamisetaRoja","CamiBuso"
+,"Sudadera","FunkoPop","KidFlash","DisfrazJunior",
+"DisfrazMujer", "Pijama","Anillos"]
+let productosPrecio=[40000,40000,40000,40000,40000,40000,40000,40000,40000,40000]
+let productosPromocion=[true,false]
+
+console.log(productosNombre)
+
+
+//Creando objetos con js
+
+let usuario={
+    nombre:"Juan",
+    edad:18,
+    amigos:["gatubela","mr frio","el pinguino"]
+}
+
+console.log(usuario)
+console.log(usuario.nombre)
+console.log(usuario.amigos[1])
---
+++ js/llenatienda.js
@@ -0,0 +1,14 @@
+let productos = [
+    {nombre: "CamisetaNegra", precio:40000,descripcion:"Camisa de color negro de The Flash "},
+    {nombre: "CamisetaRoja", precio:40000,descripcion:"Camisa de color rojo de The Flash"},
+    {nombre: "CamiBuso", precio:40000,descripcion:"Camibuso de color rojo con el logo de The Flash"},
+    {nombre: "Sudadera", precio:40000,descripcion:"Sudadera de color rojo oscuro con cierre integrado de The Flash"},
+    {nombre: "FunkoPop", precio:40000,descripcion:"FunkoPop, muñeco coleccionable de The Flash"},
+    {nombre: "KidFlash", precio:40000,descripcion:"Muñeco coleccionable Kid Flash"},
+    {nombre: "DisfrazJunior", precio:40000,descripcion:"Disfraz de The Flash para niño"},
+    {nombre: "DisfrazMujer", precio:40000,descripcion:"Disfraz de The Flash para mujer adulta"},
+    {nombre: "Pijama", precio:40000,descripcion:"Pijama de The Flash para hombre"},
+    {nombre: "Anillos", precio:40000,descripcion:"Anillos de buena calidad The Flash"},
+    
+]
+console.log(productos)
